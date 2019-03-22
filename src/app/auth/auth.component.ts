fcd5cf9631a31148f23ebadaff938effd58a4312
BiggyDev <vincent.carpentier111@gmail.com>
2019-03-22 13:14:54 +0100

    Modif Couleurs et SearchBar - Responsive

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
-import { FormsModule } from '@angular/forms';
 import * as $ from 'jquery';
 
 @Component({
