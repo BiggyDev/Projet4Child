bec9fd2da91c0f707dcc61f800556e767b96da99
BiggyDev <vincent.carpentier111@gmail.com>
2019-03-25 01:05:13 +0100

    Page d'accueil et couleurs accueil + auth

--- src/app/home/review/review.component.ts
+++ src/app/home/review/review.component.ts
@@ -9,6 +9,24 @@ export class ReviewComponent implements OnInit {
 
   constructor() { }
 
+  showCarouselOne() {
+    const two = document.getElementById('carouselTwo');
+    const one = document.getElementById('carouselOne');
+    if (one.style.display === 'none' || two.style.display === 'block') {
+      one.style.display = 'block';
+      two.style.display = 'none';
+    }
+  }
+
+  showCarouselTwo() {
+    const one = document.getElementById('carouselOne');
+    const two = document.getElementById('carouselTwo');
+    if (two.style.display === 'none' || one.style.display === 'block') {
+      one.style.display = 'none';
+      two.style.display = 'block';
+    }
+  }
+
   ngOnInit() {
   }
 
