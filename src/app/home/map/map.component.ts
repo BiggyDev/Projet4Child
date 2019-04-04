d272a42421ca46524b64f7b6cbe46bf22c64e16c
Yamna Melki <melkiyamna@protonmail.com>
2019-04-04 10:36:22 +0200

    integration leaflet map dans la page d'accueil

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -10,6 +11,11 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+          attribution: 'Frugal Map'
+      }).addTo(myfrugalmap);
   }
 
 }
