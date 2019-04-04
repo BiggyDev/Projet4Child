84b941da23cab9c7f67aee645ef6a75ec784d072
Yamna Melki <melkiyamna@protonmail.com>
2019-04-04 12:23:20 +0200

    integration leaflet map dans la page d'accueil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { ProfileProComponent } from './profiles/profile-pro/profile-pro.component';
 import { ProfilePartComponent } from './profiles/profile-part/profile-part.component';
 import { HeaderProfilesComponent } from './profiles/header-profiles/header-profiles.component';
+import {HttpClientModule} from '@angular/common/http'
 
 @NgModule({
   declarations: [
@@ -31,7 +32,9 @@ import { HeaderProfilesComponent } from './profiles/header-profiles/header-profi
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import * as L from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
 
 @Component({
   selector: 'app-map',
@@ -8,14 +10,25 @@ import * as L from 'leaflet';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient){
+    }
 
   ngOnInit() {
-      const myfrugalmap = L.map('frugalmap').setView([50.6311634, 3.0599573], 12);
+      const nannymap = L.map('map').setView([49.4431, 1.0993], 12);
 
       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: 'Frugal Map'
-      }).addTo(myfrugalmap);
+          attribution: 'Nanny Map'
+      }).addTo(nannymap);
+      const myIcon = L.icon({
+          iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+      });
+      L.marker([49.4431, 1.0993], {icon: myIcon}).bindPopup('Je suis a rouen').addTo(nannymap).openPopup();
+
+      this.http.get('https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=bornes-podotactiles').subscribe((data: any) => {
+          data.records.forEach(podotactile => {
+              L.marker([podotactile.geometry.coordinates[1], podotactile.geometry.coordinates[0]], {icon: myIcon}).addTo(nannymap);
+          });
+      });
   }
 
 }
