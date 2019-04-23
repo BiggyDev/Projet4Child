fce884447fbea8fb27286f3bd58a00a5a78878f4
Yamna Melki <melkiyamna@protonmail.com>
2019-04-23 16:58:22 +0200

    test affichage resultats recherche avec google maps parce que leaflet ca commence a me faire chier

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,40 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import * as L from 'leaflet';
-import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import { OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import {} from 'googlemaps';
 
-@Component({
-  selector: 'app-map',
-  templateUrl: './map.component.html',
-  styleUrls: ['./map.component.sass']
-})
+
 export class MapComponent implements OnInit {
-
-  constructor(private http: HttpClient){
+    @ViewChild('map') mapElement: any;
+    map: google.maps.Map;
+    ngOnInit(): void {
+        const mapProperties = {
+            center: new google.maps.LatLng(35.2271, -80.8431),
+            zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        this.map = new google.maps.Map(this.mapElement.nativeElement, mapProperties);
     }
-
-  ngOnInit() {
-      const nannymap = L.map('map').setView([49.4431, 1.0993], 12);
-
-      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-          attribution: 'Nanny Map'
-      }).addTo(nannymap);
-      const markerIcon = L.icon({
-          iconUrl: '../../../assets/img/marker.png',
-          iconSize:     [38, 38], // size of the icon
-          iconAnchor:   [22, 37], // point of the icon which will correspond to marker's location
-          shadowAnchor: [4, 62],  // the same for the shadow
-          popupAnchor:  [-3, -76]
-      });
-      const marker = L.marker([49.4431, 1.0993], {icon: markerIcon}).addTo(nannymap).openPopup();
-      marker.bindPopup('Je suis a rouen');
-
-      this.http.get('https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=bornes-podotactiles').subscribe((data: any) => {
-          data.records.forEach(podotactile => {
-              L.marker([podotactile.geometry.coordinates[1], podotactile.geometry.coordinates[0]], {icon: markerIcon}).addTo(nannymap);
-          });
-      });
-
-  }
-
 }
+// @Component({
+//   selector: 'app-map',
+//   templateUrl: './map.component.html',
+//   styleUrls: ['./map.component.sass']
+// })
+// export class MapComponent implements OnInit {
+//
+//   constructor(private http: HttpClient){
+//     }
+//
+//   ngOnInit() {
+//       const nannymap = L.map('map').setView([49.4431, 1.0993], 12);
+//
+//       L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+//           attribution: 'Nanny Map'
+//       }).addTo(nannymap);
+//       const markerIcon = L.icon({
+//           iconUrl: '../../../assets/img/marker.png',
+//           iconSize:     [38, 38], // size of the icon
+//           iconAnchor:   [22, 37], // point of the icon which will correspond to marker's location
+//           shadowAnchor: [4, 62],  // the same for the shadow
+//           popupAnchor:  [-3, -76]
+//       });
+//       const marker = L.marker([49.4431, 1.0993], {icon: markerIcon}).addTo(nannymap).openPopup();
+//       marker.bindPopup('Je suis a rouen');
+//
+//       this.http.get('https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=bornes-podotactiles').subscribe((data: any) => {
+//           data.records.forEach(podotactile => {
+//               L.marker([podotactile.geometry.coordinates[1], podotactile.geometry.coordinates[0]], {icon: markerIcon}).addTo(nannymap);
+//           });
+//       });
+//
+//   }
+//
+// }
